bbe24172abaa3ed17712d45e706a3d959a7747c5
sele14 <eirik.selander@gmail.com>
2020-12-26 17:05:36 +0100

    str rep

--- invest_app/api/models.py
+++ invest_app/api/models.py
@@ -9,3 +9,7 @@ class Investments(models.Model):
     asset_name = models.CharField(max_length=20, default="", unique=False)
     asset_quantity = models.IntegerField(default=0)
     asset_price = models.FloatField(default=0)
+
+    # Adding a string representation of the model to display the contents in human-readable form
+    def __str__(self):
+        return self.asset_type
